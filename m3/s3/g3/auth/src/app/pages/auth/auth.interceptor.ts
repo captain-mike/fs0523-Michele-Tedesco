a47b333e7876d83db10b1415d607de2268ce9050
captain-mike <info@grafikweb.it>
2023-12-06 23:01:08 +0100

    commenti

--- m3/s3/g3/auth/src/app/pages/auth/auth.interceptor.ts
+++ m3/s3/g3/auth/src/app/pages/auth/auth.interceptor.ts
@@ -1,5 +1,33 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { iAccessData } from './Models/i-access-data';
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, switchMap } from 'rxjs';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(
+    private authSvc:AuthService
+  ) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    return this.authSvc.user$.pipe(switchMap((user:iAccessData|null) => {
+
+      if(!user) return next.handle(request);
+
+      const newRequest = request.clone({
+        headers: request.headers.append('Authorization','Bearer '+ user.accessToken)
+      })
+
+      return next.handle(newRequest);
+    }))
+
+  }
+}
--- m3/s3/g3/auth/src/app/pages/auth/auth.service.ts
+++ m3/s3/g3/auth/src/app/pages/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { environment } from '../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { iRegister } from './Models/register';
 import { iAccessData } from './Models/i-access-data';
-import { BehaviorSubject, Observable, map, tap } from 'rxjs';
+import { BehaviorSubject, Observable, map, tap, throwError } from 'rxjs';
 import { iLogin } from './Models/login';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router } from '@angular/router';
@@ -13,22 +13,24 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  jwtHelper:JwtHelperService = new JwtHelperService()
-  authSubject = new BehaviorSubject<iAccessData|null>(null);
+  jwtHelper:JwtHelperService = new JwtHelperService()//ci permette di lavorare facilmente con i jwt
+
+  authSubject = new BehaviorSubject<iAccessData|null>(null);//null è il valore di default, quindi si parte con utente non loggato
 
   user$ = this.authSubject.asObservable();//contiene i dati dell'utente loggato oppure null
   isLoggedIn$ = this.user$.pipe(map(user => !!user))//fornisce true o false in base allo stato di autenticaziuone dell'utente
   //isLoggedIn$ = this.user$.pipe(map(user => Boolean(user)))
 
   constructor(
-    private http:HttpClient,
-    private router:Router
+    private http: HttpClient,//per le chiamate http
+    private router: Router//per i redirect
   ) {
 
-    this.restoreUser()
+    this.restoreUser()//come prima cosa controllo se è già attiva una sessione, e la ripristino
 
   }
 
+  //ng g environment
   registerUrl:string = environment.apiUrl + '/register';
   loginUrl:string = environment.apiUrl + '/login'
 
@@ -51,31 +53,53 @@ export class AuthService {
   }
 
   autoLogout(jwt:string){
-    const expDate = this.jwtHelper.getTokenExpirationDate(jwt) as Date;
-    const expMs = expDate.getTime() - new Date().getTime();
+    const expDate = this.jwtHelper.getTokenExpirationDate(jwt) as Date;//recupero la data di scadenza del jwt
+    const expMs = expDate.getTime() - new Date().getTime();//sottraggo i ms della data attuale da quelli della data del jwt
 
-    setTimeout(()=>{
+    setTimeout(()=>{//avvio un timer che fa logout allo scadere del tempo
       this.logout()
     },expMs)
   }
 
   logout(){
-    this.authSubject.next(null);
-    localStorage.removeItem('accessData');
-    this.router.navigate(['/auth/login']);
+    this.authSubject.next(null);//comunico al behaviorsubject che il valore da propagare è null
+    localStorage.removeItem('accessData');//elimino i dati salvati in localstorage
+    this.router.navigate(['/auth/login']);//redirect al login
   }
 
+  //metodo che controlla al reload di pagina se l'utente è loggato e se il jwt è scaduto
   restoreUser(){
 
-      const userJson:string|null =  localStorage.getItem('accessData');
-      if(!userJson) return;
+      const userJson:string|null =  localStorage.getItem('accessData');//recupero i dati di accesso
+      if(!userJson) return;//se i dati non ci sono blocco la funzione
 
-      const accessData:iAccessData = JSON.parse(userJson);
-      if(this.jwtHelper.isTokenExpired(accessData.accessToken)) return;
+      const accessData:iAccessData = JSON.parse(userJson);//se viene eseguita questa riga significa che i dati ci sono, quindi converto la stringa(che conteneva un json) in oggetto
+      if(this.jwtHelper.isTokenExpired(accessData.accessToken)) return;//ora controllo se il token è scaduto, se lo è fermiamo la funzione
 
-      this.autoLogout(accessData.accessToken)
-      this.authSubject.next(accessData)
+      //se nessun return viene eseguito proseguo
+      this.authSubject.next(accessData)//invio i dati dell'utente al behaviorsubject
+      this.autoLogout(accessData.accessToken)//riavvio il timer per la scadenza della sessione
   }
 
+
+  errors(err: any) {
+    switch (err.error) {
+        case "Email and Password are required":
+            return new Error('Email e password obbligatorie');
+            break;
+        case "Email already exists":
+            return new Error('Utente esistente');
+            break;
+        case 'Email format is invalid':
+            return new Error('Email scritta male');
+            break;
+        case 'Cannot find user':
+            return new Error('utente inesistente');
+            break;
+            default:
+        return new Error('Errore');
+            break;
+    }
+  }
 
 }
